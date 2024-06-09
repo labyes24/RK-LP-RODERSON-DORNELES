ccd2dd0aa0a266a85946236fcb8d0fc80f710635
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2024-06-09 16:44:07 -0300

    chore/RLRD-17: (data) Apply i18n to property data

--- src/data/RealStateData.js
+++ src/data/RealStateData.js
@@ -141,7 +141,121 @@ const propertiesPT = [
 
 /**  @type {Array<Property>} The Portuguese properties data */
 const propertiesES = [
-  // TODO: Add Spanish properties text
+  {
+    id: crypto.randomUUID(),
+    brokerId: BROKER_ID,
+    RKCode: '9527',
+    title: 'Jurerê Iate',
+    region: 'Jurerê',
+    description:
+      '**INMUEBLE CON MATRÍCULA**\n\n¡Oportunidad única para vivir en Jurerê Iate Residence! Usted merece lo mejor de la vida y ahora puede conquistarlo con gran estilo. Presentamos un apartamento de ensueño. Alto Estándar en Jurerê, listo para ofrecer el máximo confort y sofisticación para usted y su familia. Perfecto para alojar a su familia con comodidad y elegancia. ¡Amueblado con muy buen gusto!',
+    price: 'R$ 3.500.000,00',
+    details: {
+      square: '185,00 m²',
+      rooms: '3',
+      bathrooms: '4',
+      garage: '2',
+    },
+    pictures: [
+      './properties/jurere-f01.jpg',
+      './properties/jurere-f02.jpg',
+      './properties/jurere-f03.jpg',
+      './properties/jurere-f04.jpg',
+      './properties/jurere-f05.jpg',
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    brokerId: BROKER_ID,
+    RKCode: '9735',
+    title: 'Casa Alto Padrão',
+    region: 'Jurerê Internacional',
+    description:
+      '**INMUEBLE CON MATRÍCULA**\n\n¡Oportunidad única para vivir en Jurerê Iate Residence! Usted merece lo mejor de la vida y ahora puede conquistarlo con gran estilo. Presentamos un apartamento de ensueño. Alto Estándar en Jurerê, listo para ofrecer el máximo confort y sofisticación para usted y su familia. Perfecto para alojar a su familia con comodidad y elegancia. ¡Amueblado con muy buen gusto!',
+    price: 'R$ 3.500.000,00',
+    details: {
+      square: '185,00 m²',
+      rooms: '3',
+      bathrooms: '4',
+      garage: '2',
+    },
+    pictures: [
+      './properties/jurere-f01.jpg',
+      './properties/jurere-f02.jpg',
+      './properties/jurere-f03.jpg',
+      './properties/jurere-f04.jpg',
+      './properties/jurere-f05.jpg',
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    brokerId: BROKER_ID,
+    RKCode: '9735',
+    title: 'Casa Alto Padrão',
+    region: 'Jurerê Internacional',
+    description:
+      '**INMUEBLE CON MATRÍCULA**\n\nUbicada en el corazón de Jurerê Internacional, esta propiedad deslumbrante combina elegancia, entretenimiento y ocio en una experiencia única. Con características distintivas, destacamos: Espacio Acogedor, Área Exterior Relajante, Cocina Moderna, Ubicación Privilegiada y Mobiliario de Lujo.',
+    price: 'R$ 7.900.000,00',
+    details: {
+      square: '445,96 m²',
+      rooms: '6',
+      bathrooms: '5',
+      garage: '4',
+    },
+    pictures: [
+      './properties/casalto-f01.jpg',
+      './properties/casalto-f02.jpg',
+      './properties/casalto-f03.jpg',
+      './properties/casalto-f04.jpg',
+      './properties/casalto-f05.jpg',
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    brokerId: BROKER_ID,
+    RKCode: '9830',
+    title: 'Vl. das Gaivotas',
+    region: 'Ingleses',
+    description:
+      '**INMUEBLE CON MATRÍCULA**\n\nApartamento a 200m del mar, amueblado y soleado, con trastero y garaje. Excelente oportunidad para aquellos que desean vivir cerca del mar o invertir en alquileres de temporada. Ubicado en una de las mejores calles del barrio, Costão Norte, condominio con piscina, ascensor, gimnasio, quiosco con parrillas y arbolado',
+    price: 'R$ 615.000,00',
+    details: {
+      square: '68 m²',
+      rooms: '2',
+      bathrooms: '2',
+      garage: '1',
+    },
+    pictures: [
+      './properties/vlgaivotas-f01.jpg',
+      './properties/vlgaivotas-f02.jpg',
+      './properties/vlgaivotas-f03.jpg',
+      './properties/vlgaivotas-f04.jpg',
+      './properties/vlgaivotas-f05.jpg',
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    brokerId: BROKER_ID,
+    RKCode: '7646',
+    title: 'Holiday',
+    region: 'Ingleses',
+    description:
+      'Maravillosa cobertura con vista plena al mar. Ubicada en la zona más exclusiva de la playa de Ingleses. Inversión con retorno garantizado en alquileres de temporada o para realizar el sueño de vivir frente al mar. Totalmente amueblada y decorada con mucho gusto y detalles en todos los ambientes.\n\n**Inmueble en Proceso de**\n\n**Individualización de Matrícula**',
+    price: 'R$ 2.890.000,00',
+    details: {
+      square: '300,26 m²',
+      rooms: '4',
+      bathrooms: '5',
+      garage: '2',
+    },
+    pictures: [
+      './properties/holiday-f01.jpg',
+      './properties/holiday-f02.jpg',
+      './properties/holiday-f03.jpg',
+      './properties/holiday-f04.jpg',
+      './properties/holiday-f05.jpg',
+    ],
+  },
 ]
 
 /**
@@ -154,7 +268,7 @@ const propertiesES = [
 /** @type {PropertiesData} The properties data */
 const properties = {
   pt: propertiesPT,
-  // es: propertiesES,
+  es: propertiesES,
 }
 
 /**
